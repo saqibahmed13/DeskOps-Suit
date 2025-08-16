858b425404507fe58f1b36ad529a7742ced2e403
Saqibahmed13 <saqibcode@gmail.com>
2025-08-16 17:30:38 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
             }
           />
           <Route
-            path="/addon"
+            path="/Addon"
             element={
               <Addon
                 customer={customers[activeCustomerIndex]}
